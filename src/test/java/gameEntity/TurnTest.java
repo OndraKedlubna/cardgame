121d96e8dc61329b694175d0e53c2ebd3bc82084
OndraG <ondra.to.stejne.necte@seznam.cz>
2017-04-05 22:11:59 +0200

    TurnTest almost complete

--- src/test/java/gameEntity/TurnTest.java
+++ src/test/java/gameEntity/TurnTest.java
@@ -5,14 +5,15 @@ import static org.junit.Assert.assertEquals;
 import java.util.Arrays;
 import java.util.List;
 
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import card.ICard;
 import enums.Action;
 import enums.AnswerType;
 
@@ -22,27 +23,37 @@ public class TurnTest {
 	@InjectMocks
 	private Turn testInstance;
 	
+	@Mock
     private Player player;
+	
+	@Mock
+    private Player player1;
+	
+	@Mock
+    private Player player2;
+	
+	@Mock
+    private Player player3;
     
     private List<Player> players;
-
-    public TurnTest() {
-    }
-
-    @BeforeClass
-    public static void setUpClass() {
-    }
-
-    @AfterClass
-    public static void tearDownClass() {
-    }
+    
+    @Mock
+    private ICard card;
+    
+    @Mock
+    private ICard card2;
+    
+    @Mock
+    private RawMaterial universalTestMaterial;
+    
+    @Mock
+    private RawMaterial universalTestMaterial2;
 
     @Before
     public void setUp() {
         testInstance = new Turn();
-        //player = new PlayerBuilder().buildPlayer("Alex", null, true);
-        //players = Arrays.asList(player);
-        //player.getHand().add(new RedEnergy());
+        players = Arrays.asList(player1, player2, player3);
+        Mockito.when(player.getHand()).thenReturn(Arrays.asList(card, card2));
     }
 //TODO
     @Test
@@ -65,11 +76,11 @@ public class TurnTest {
                 testInstance.getAnswer().getText());
     }
     
-  //TODO
     @Test
     public void testPlayNotResourceReject() {
-        player.getGold().setAmount(0);
+    	initMaterials();
         testInstance.setIdCard(1);
+        Mockito.when(player.getGold()).thenReturn(universalTestMaterial2);
         testInstance.setAction(Action.PLAY);
         testInstance.doAction(player, players);
         assertEquals(AnswerType.REJECT, testInstance.getAnswer().getType());
@@ -77,10 +88,9 @@ public class TurnTest {
                 testInstance.getAnswer().getText());
     }
 
-  //TODO
     @Test
     public void testThrowActionReject() {
-        testInstance.setIdCard(2);
+        testInstance.setIdCard(4);
         testInstance.setAction(Action.THROW);
         testInstance.doAction(player, players);
         assertEquals(AnswerType.REJECT, testInstance.getAnswer().getType());
@@ -88,12 +98,51 @@ public class TurnTest {
                 testInstance.getAnswer().getText());
     }
 
-  //TODO
     @Test
     public void testCost() {
-        //assertEquals(true, testInstance.compareValue(player, new RedEnergy()));
-        player.getGold().setAmount(0);
-        //assertEquals(false, testInstance.compareValue(player, new RedEnergy()));
+    	initMaterials();
+        assertEquals(true, testInstance.compareValue(player, card));
+    }
+    
+    @Test
+    public void testCostFailRed() {
+    	initMaterials();
+    	Mockito.when(player.getRedMana()).thenReturn(universalTestMaterial2);
+        assertEquals(false, testInstance.compareValue(player, card));
+    }
+    
+    @Test
+    public void testCostFailBlue() {
+    	initMaterials();
+    	Mockito.when(player.getBlueMana()).thenReturn(universalTestMaterial2);
+        assertEquals(false, testInstance.compareValue(player, card));
+    }
+    
+    @Test
+    public void testCostFailGreen() {
+    	initMaterials();
+    	Mockito.when(player.getGreenMana()).thenReturn(universalTestMaterial2);
+        assertEquals(false, testInstance.compareValue(player, card));
+    }
+    
+    @Test
+    public void testCostFailGold() {
+    	initMaterials();
+    	Mockito.when(player.getGold()).thenReturn(universalTestMaterial2);
+        assertEquals(false, testInstance.compareValue(player, card));
+    }
+    
+    private void initMaterials(){
+    	Mockito.when(player.getGold()).thenReturn(universalTestMaterial);
+    	Mockito.when(player.getBlueMana()).thenReturn(universalTestMaterial);
+    	Mockito.when(player.getRedMana()).thenReturn(universalTestMaterial);
+    	Mockito.when(player.getGreenMana()).thenReturn(universalTestMaterial);
+    	Mockito.when(card.blueCost()).thenReturn(2);
+    	Mockito.when(card.redCost()).thenReturn(2);
+    	Mockito.when(card.greenCost()).thenReturn(2);
+    	Mockito.when(card.goldCost()).thenReturn(2);
+    	Mockito.when(universalTestMaterial.getAmount()).thenReturn(3);
+    	Mockito.when(universalTestMaterial2.getAmount()).thenReturn(1);
     }
 
 }
