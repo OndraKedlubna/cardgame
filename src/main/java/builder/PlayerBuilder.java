e0f5ac106971e3071b7d82808e39629a9b4b2213
OndraG <ondra.to.stejne.necte@seznam.cz>
2017-04-14 20:31:37 +0200

    Add PlayerBuilder

---
+++ src/main/java/builder/PlayerBuilder.java
@@ -0,0 +1,40 @@
+package builder;
+
+import java.util.ArrayList;
+
+import gameEntity.Player;
+import gameEntity.RawMaterial;
+
+/**
+ * Builder na vytvareni novych hracu
+ */
+public class PlayerBuilder {
+	
+	public Player buildPlayer(String name, Player next, boolean start){
+        Player p = new Player();
+        p.setName(name);
+        p.setNextPlayer(next);
+        p.setStarter(start);
+        p.setHand(new ArrayList<>());
+        p.setDeletePack(new ArrayList<>());
+        p.setPack(new ArrayList<>());
+        initRaws(p);
+        return p;
+    }
+    
+    private void initRaws(Player p){
+        p.setGold(buildRaw("gold", 50, 5, 1));
+        p.setBlueMana(buildRaw("BM", 50, 5, 1));
+        p.setGreenMana(buildRaw("GM", 50, 5, 1));
+        p.setRedMana(buildRaw("RM", 50, 5, 1));
+    }
+    
+    private RawMaterial buildRaw(String name, int max, int am, int grow){
+        RawMaterial r = new RawMaterial();
+        r.setName(name);
+        r.setAmount(am);
+        r.setMaxAmount(max);
+        r.setGrow(grow);
+        return r;
+    }
+}
---
+++ src/test/java/builder/PlayerBuilderTest.java
@@ -0,0 +1,49 @@
+package builder;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import gameEntity.Player;
+
+@RunWith(MockitoJUnitRunner.class)
+public class PlayerBuilderTest {
+
+	@InjectMocks
+	private static PlayerBuilder testInstance;
+
+	@Test
+	public void buildPlayerTest() {
+		Player p = testInstance.buildPlayer("alex", null, true);
+		Player p2 = testInstance.buildPlayer("franta", p, false);
+		assertEquals(p.getName(), "alex");
+		assertEquals(p.isStarter(), true);
+		assertEquals(p2.getNextPlayer(), p);
+		assertEquals(p.getHand().size(), 0);
+
+		assertEquals(p.getGold().getName(), "gold");
+		assertEquals(p.getGold().getMaxAmount(), 50);
+		assertEquals(p.getGold().getAmount(), 5);
+		assertEquals(p.getGold().getGrow(), 1);
+
+		assertEquals(p.getBlueMana().getName(), "BM");
+		assertEquals(p.getBlueMana().getMaxAmount(), 50);
+		assertEquals(p.getBlueMana().getAmount(), 5);
+		assertEquals(p.getBlueMana().getGrow(), 1);
+
+		assertEquals(p.getRedMana().getName(), "RM");
+		assertEquals(p.getRedMana().getMaxAmount(), 50);
+		assertEquals(p.getRedMana().getAmount(), 5);
+		assertEquals(p.getRedMana().getGrow(), 1);
+
+		assertEquals(p.getGreenMana().getName(), "GM");
+		assertEquals(p.getGreenMana().getMaxAmount(), 50);
+		assertEquals(p.getGreenMana().getAmount(), 5);
+		assertEquals(p.getGreenMana().getGrow(), 1);
+	}
+
+}
