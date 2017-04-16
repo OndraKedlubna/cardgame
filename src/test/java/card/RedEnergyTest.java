2e83362566309ee4062d05c3e7785b9e7c9e2e9a
OndraG <ondra.to.stejne.necte@seznam.cz>
2017-04-16 18:15:26 +0200

    Re testing

--- src/test/java/card/RedEnergyTest.java
+++ src/test/java/card/RedEnergyTest.java
@@ -4,12 +4,15 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import gameEntity.Player;
+import gameEntity.RawMaterial;
 
 @RunWith(MockitoJUnitRunner.class)
 public class RedEnergyTest {
@@ -29,11 +32,23 @@ public class RedEnergyTest {
 	@Mock
 	private Player player4;
 	
+	@Mock
+	private RawMaterial redMana;
+	
 	private List<Player> players;
 	
 	@Before
 	public void setUp(){
+		Mockito.when(player1.getName()).thenReturn("Alex");
 		players = Arrays.asList(player1, player2, player3, player4);
+	}
+	
+	@Test
+	public void testEfect(){
+		Mockito.when(player1.getRedMana()).thenReturn(redMana);
+		String result = testInstance.doEffect(player1, players);
+		Mockito.verify(redMana).increase(5);
+		System.out.println(result);
 	}
 
 }
